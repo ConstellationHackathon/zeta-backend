8185107956b32a3311664fa743c428003101d261
Sebas <sheva3232@gmail.com>
2023-12-05 21:21:16 -0500

    :zap: Improve receiver code and security

--- contracts/ZetaCCIPReceiver.sol
+++ contracts/ZetaCCIPReceiver.sol
@@ -6,16 +6,34 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 
-contract ZetaCCIPReceiver is CCIPReceiver, ReentrancyGuard {
+contract ZetaCCIPReceiver is CCIPReceiver, ReentrancyGuard, Ownable {
     AggregatorV3Interface internal priceFeed;
 
-    event FundsTransferred(address indexed recipient, uint256 indexed amount);
-    event LogFound(address userAddress, uint256 amountToTransfer);
+    event MsgReceived(
+        address indexed userAddress,
+        uint256 indexed amountToTransferUSD,
+        uint256 indexed timestamp
+    );
+    event ETHToSend(
+        address indexed userAddress,
+        uint256 indexed amountToTransferUSD,
+        uint256 indexed timestamp
+    );
+    event FundsTransferred(
+        address indexed recipient,
+        uint256 indexed amount,
+        uint256 indexed timestamp
+    );
+    event ConfigurationChanged(
+        string indexed config,
+        address indexed oldValue,
+        address indexed newValue,
+        uint256 timestamp
+    );
+    
+    address public EthUsdAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
 
-    address public EthUsdAddress =
-        0x694AA1769357215DE4FAC081bf1f309aDC325306;
-
-    constructor(address router) CCIPReceiver(router) {
+    constructor(address router) CCIPReceiver(router) Ownable(msg.sender) {
         priceFeed = AggregatorV3Interface(EthUsdAddress);
     }
 
@@ -27,20 +45,17 @@ contract ZetaCCIPReceiver is CCIPReceiver, ReentrancyGuard {
             message.data,
             (address, uint256)
         );
-        emit LogFound(userAddress, userAmount);
+        emit MsgReceived(userAddress, userAmount, block.timestamp);
         uint amountToTransfer = userAmount / EthUsdPrice;
-        emit LogFound(userAddress, amountToTransfer);
-
         require(userAddress != address(0), "Invalid recipient address");
         require(
             amountToTransfer <= address(this).balance,
             "Smart contract does not have enough balance"
         );
-
+        emit ETHToSend(userAddress, amountToTransfer, block.timestamp);
         (bool success, ) = userAddress.call{value: amountToTransfer}("");
         require(success, "Transfer failed");
-
-        emit FundsTransferred(userAddress, amountToTransfer);
+        emit FundsTransferred(userAddress, amountToTransfer, block.timestamp);
     }
 
     // Function to allow the contract to receive ether
@@ -60,6 +75,16 @@ contract ZetaCCIPReceiver is CCIPReceiver, ReentrancyGuard {
 
     function getContractBalance() external view returns (uint) {
         return address(this).balance;
+    }
+    function updateEthUsdAddress(address newAddress) external onlyOwner {
+        EthUsdAddress = newAddress;
+        priceFeed = AggregatorV3Interface(EthUsdAddress);
+        emit ConfigurationChanged(
+            "ETHUsdAddress",
+            EthUsdAddress,
+            newAddress,
+            block.timestamp
+        );
     }
 }
 
