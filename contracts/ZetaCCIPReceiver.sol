1cd8094fb23cb2bd5887e90da9fd63ab263c0b03
Sebas <sheva3232@gmail.com>
2023-12-05 20:36:53 -0500

    :zap: Improving events and format on sender avax contract

--- contracts/ZetaCCIPReceiver.sol
+++ contracts/ZetaCCIPReceiver.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 
 contract ZetaCCIPReceiver is CCIPReceiver, ReentrancyGuard {
     AggregatorV3Interface internal priceFeed;
-    address public constant EthUsdAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+    address public constant EthUsdAddress =
+        0x694AA1769357215DE4FAC081bf1f309aDC325306;
     event FundsTransferred(address indexed recipient, uint256 indexed amount);
     event LogFound(address userAddress, uint256 amountToTransfer);
 
@@ -15,15 +16,23 @@ contract ZetaCCIPReceiver is CCIPReceiver, ReentrancyGuard {
         priceFeed = AggregatorV3Interface(EthUsdAddress);
     }
 
-    function _ccipReceive(Client.Any2EVMMessage memory message) internal override  {
-        uint EthUsdPrice =  uint(getPrice()); 
-        (address userAddress, uint256 userAmount) = abi.decode(message.data, (address, uint256));
+    function _ccipReceive(
+        Client.Any2EVMMessage memory message
+    ) internal override {
+        uint EthUsdPrice = uint(getPrice());
+        (address userAddress, uint256 userAmount) = abi.decode(
+            message.data,
+            (address, uint256)
+        );
         emit LogFound(userAddress, userAmount);
         uint amountToTransfer = userAmount / EthUsdPrice;
         emit LogFound(userAddress, amountToTransfer);
 
         require(userAddress != address(0), "Invalid recipient address");
-        require(amountToTransfer <= address(this).balance, "Smart contract does not have enough balance");
+        require(
+            amountToTransfer <= address(this).balance,
+            "Smart contract does not have enough balance"
+        );
 
         (bool success, ) = userAddress.call{value: amountToTransfer}("");
         require(success, "Transfer failed");
@@ -44,6 +53,9 @@ contract ZetaCCIPReceiver is CCIPReceiver, ReentrancyGuard {
         ) = priceFeed.latestRoundData();
         // for ETH / USD price is scaled up by 10 ** 8
         return price / 1e8;
+    }
+    function getContractBalance() external view returns (uint) {
+        return address(this).balance;
     }
 }
 
--- contracts/ZetaCCIPSender.sol
+++ contracts/ZetaCCIPSender.sol
@@ -9,25 +9,58 @@ import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.s
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
 
-
-contract ZetaCCIPSender  {
-    address public constant AvaxUsdAddress = 0x5498BB86BC934c8D34FDA08E81D444153d0D06aD;
+contract ZetaCCIPSender {
     AggregatorV3Interface internal priceFeed;
-    event TokenReceived(address indexed sender, uint indexed amount);
-    event FundsWithdrawn(address indexed withdrawnBy, uint indexed amount);
-    event ConfigurationChanged(string indexed config, address indexed oldValue, address indexed newValue);
-    event ChainSelectorChanged(uint64 indexed oldSelector, uint64  indexed newSelector);
-    event Log(address Sender, uint amount);
 
-    using Strings for address;
-    using Strings for uint;
+    event AvaxReceived(
+        address indexed sender,
+        uint256 indexed tokenAmount,
+        uint256 indexed timestamp
+    );
+    event AvaxPriceInUSD(
+        address indexed sender,
+        uint256 indexed price,
+        uint256 indexed timestamp
+    );
+    event AmountSentInUSD(
+        address indexed sender,
+        uint256 indexed amountSent,
+        uint256 indexed timestamp
+    );
+    event FundsWithdrawn(
+        address indexed withdrawnBy,
+        uint indexed amount,
+        uint256 indexed timestamp
+    );
 
+    event ConfigurationChanged(
+        string indexed config,
+        address indexed oldValue,
+        address indexed newValue,
+        uint256 timestamp
+    );
+    event ChainSelectorChanged(
+        uint64 indexed oldSelector,
+        uint64 indexed newSelector,
+        uint256 indexed timestamp
+    );
+
+    address public constant AvaxUsdAddress =
+        0x5498BB86BC934c8D34FDA08E81D444153d0D06aD;
     address link;
     address router;
     address receiver;
     uint64 destinationChainSelector;
 
-    constructor(address  _receiver, address _link, address _router, uint64 _destinationChainSelector) {
+    using Strings for address;
+    using Strings for uint;
+
+    constructor(
+        address _receiver,
+        address _link,
+        address _router,
+        uint64 _destinationChainSelector
+    ) {
         // _receiver Smart Contract receiver address
         // _link CCIP link address
         // _router CCIP router address
@@ -42,16 +75,15 @@ contract ZetaCCIPSender  {
 
     // The receive function to handle receiving Ether
     receive() external payable {
-        emit TokenReceived(msg.sender, msg.value);
-
         handleIncomingToken(msg.sender, msg.value);
     }
 
-
     function handleIncomingToken(address sender, uint amount) internal {
-        uint AvaxUsdPrice =  uint(getPrice()); 
-        emit Log(sender, AvaxUsdPrice);
-        uint amountToTransfer = amount * AvaxUsdPrice;
+        emit AvaxReceived(sender, amount, block.timestamp);
+        uint avaxUsdPrice = uint(getPrice());
+        emit AvaxPriceInUSD(sender, avaxUsdPrice, block.timestamp);
+        uint amountToTransfer = amount * avaxUsdPrice;
+        emit AmountSentInUSD(sender, amountToTransfer, block.timestamp);
 
         bytes memory messageContent = abi.encode(sender, amountToTransfer);
         Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
@@ -65,38 +97,60 @@ contract ZetaCCIPSender  {
     }
 
     // Function to withdraw the funds from the contract, accessible only by the owner
-    function withdrawFunds() external  {
+    function withdrawFunds() external {
         uint balance = address(this).balance;
-        require(balance > 0, "The smart contract does not have enough founds to withdraw");
+        require(
+            balance > 0,
+            "The smart contract does not have enough founds to withdraw"
+        );
 
         // Transfer all the funds to the owner's address
         (bool success, ) = msg.sender.call{value: balance}("");
         require(success, "Send ");
 
-        emit FundsWithdrawn(msg.sender, balance);
+        emit FundsWithdrawn(msg.sender, balance, block.timestamp);
     }
 
     // Function to update contract configurations, accessible only by the owner
-        function updateConfiguration(string memory config, address newValue) external  {
-            if (keccak256(bytes(config)) == keccak256(bytes("link"))) {
-                emit ConfigurationChanged(config, link, newValue);
-                link = newValue;
-            } else if (keccak256(bytes(config)) == keccak256(bytes("router"))) {
-                emit ConfigurationChanged(config, router, newValue);
-                router = newValue;
-            } else if (keccak256(bytes(config)) == keccak256(bytes("msgReceiver"))) {
-                emit ConfigurationChanged(config, receiver, newValue);
-                receiver = newValue;
-            } 
+    function updateConfiguration(
+        string memory config,
+        address newValue
+    ) external {
+        if (keccak256(bytes(config)) == keccak256(bytes("link"))) {
+            emit ConfigurationChanged(config, link, newValue, block.timestamp);
+            link = newValue;
+        } else if (keccak256(bytes(config)) == keccak256(bytes("router"))) {
+            emit ConfigurationChanged(
+                config,
+                router,
+                newValue,
+                block.timestamp
+            );
+            router = newValue;
+        } else if (
+            keccak256(bytes(config)) == keccak256(bytes("msgReceiver"))
+        ) {
+            emit ConfigurationChanged(
+                config,
+                receiver,
+                newValue,
+                block.timestamp
+            );
+            receiver = newValue;
         }
+    }
 
     // Function to update destination chain selector, accessible only by the owner
     function updateDestinationChainSelector(uint64 newSelector) external {
-        emit ChainSelectorChanged(destinationChainSelector, newSelector);
+        emit ChainSelectorChanged(
+            destinationChainSelector,
+            newSelector,
+            block.timestamp
+        );
         destinationChainSelector = newSelector;
     }
 
-    function getPrice() public view returns (int) {
+    function getPrice() internal view returns (int) {
         (
             uint80 roundID,
             int price,
